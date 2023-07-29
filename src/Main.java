a4d3869381ea5d669e21afeb882ffced79ff2c5c
OctavianG <octavian7.g@gmail.com>
2023-07-29 13:21:38 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
     // Explanation: Your function should return k = 5, with the first five elements of nums
     // being 0, 1, 2, 3, and 4 respectively.
     // It does not matter what you leave beyond the returned k (hence they are underscores).
+
+    // Time complexity O(n) and space complexity O(1)
     public static int removeDuplicates(int[] nums) {
         int k = 1;
 
